d72d8e5461953018cba391c899873503867d4c7e
Dane, Lee <sleepywood705@gmail.com>
2024-12-18 20:16:58 +0900

    20241218
    1. MBTI 이미지 삽입

--- fe/src/Browser/Pharagraph/Component/Pharagraph_MBTISearch.js
+++ fe/src/Browser/Pharagraph/Component/Pharagraph_MBTISearch.js
@@ -10,22 +10,22 @@ export function PharagraphMBTISearch({ MBTI, setFormData }) {
   return (
     <div id="PharagraphMBTISearch">
       <div>
-        <button onClick={() => handleSelect("ESTJ")}>ESTJ</button>
-        <button onClick={() => handleSelect("ESFJ")}>ESFJ</button>
-        <button onClick={() => handleSelect("ENTJ")}>ENTJ</button>
-        <button onClick={() => handleSelect("ENFJ")}>ENFJ</button>
-        <button onClick={() => handleSelect("ESTP")}>ESTP</button>
-        <button onClick={() => handleSelect("ESFP")}>ESFP</button>
-        <button onClick={() => handleSelect("ENTP")}>ENTP</button>
-        <button onClick={() => handleSelect("ENFP")}>ENFP</button>
-        <button onClick={() => handleSelect("ISTJ")}>ISTJ</button>
-        <button onClick={() => handleSelect("ISFJ")}>ISFJ</button>
-        <button onClick={() => handleSelect("INTJ")}>INTJ</button>
-        <button onClick={() => handleSelect("INFJ")}>INFJ</button>
-        <button onClick={() => handleSelect("ISTP")}>ISTP</button>
-        <button onClick={() => handleSelect("ISFP")}>ISFP</button>
-        <button onClick={() => handleSelect("INTP")}>INTP</button>
-        <button onClick={() => handleSelect("INFP")}>INFP</button>
+        <button onClick={() => handleSelect("ESTJ")} className="ESTJ">ESTJ</button>
+        <button onClick={() => handleSelect("ESFJ")} className="ESFJ">ESFJ</button>
+        <button onClick={() => handleSelect("ENTJ")} className="ENTJ">ENTJ</button>
+        <button onClick={() => handleSelect("ENFJ")} className="ENFJ">ENFJ</button>
+        <button onClick={() => handleSelect("ESTP")} className="ESTP">ESTP</button>
+        <button onClick={() => handleSelect("ESFP")} className="ESFP">ESFP</button>
+        <button onClick={() => handleSelect("ENTP")} className="ENTP">ENTP</button>
+        <button onClick={() => handleSelect("ENFP")} className="ENFP">ENFP</button>
+        <button onClick={() => handleSelect("ISTJ")} className="ISTJ">ISTJ</button>
+        <button onClick={() => handleSelect("ISFJ")} className="ISFJ">ISFJ</button>
+        <button onClick={() => handleSelect("INTJ")} className="INTJ">INTJ</button>
+        <button onClick={() => handleSelect("INFJ")} className="INFJ">INFJ</button>
+        <button onClick={() => handleSelect("ISTP")} className="ISTP">ISTP</button>
+        <button onClick={() => handleSelect("ISFP")} className="ISFP">ISFP</button>
+        <button onClick={() => handleSelect("INTP")} className="INTP">INTP</button>
+        <button onClick={() => handleSelect("INFP")} className="INFP">INFP</button>
       </div>
     </div>
   );
